36bd99148e7ffc38920acc40491459d9ffb071a2
François Beiger <fbeiger971@gmail.com>
2017-10-27 14:48:56 -0400

    fix several issues

--- LOG4420_TP3/assets/js/shopping-cart.js
+++ LOG4420_TP3/assets/js/shopping-cart.js
@@ -26,9 +26,9 @@ $(document).ready(function() {
       $(".shopping-cart-table tbody").append("'<tr data-id='"+product.id+"'>'"+
             "<td class='first-column'><button class='shopping-cart-button remove-item-button'>x</button></td>"+
             "<td><a href='./product.html'>"+product.name+"</a></td>"+
-            "<td class='price'>"+product.price.toCurrencyString()+"&thinsp;$</td>"+
+            "<td>"+product.price.toCurrencyString()+"&thinsp;$</td>"+
             "<td><button class='shopping-cart-button remove-quantity-button' "+disabled+">-</button><span class='quantity'>"+product.quantity+"</span><button class='shopping-cart-button add-quantity-button'>+</button></td>"+
-            "<td class='last-column' id='total-amount'>"+(product.price*product.quantity).toFixed(2).toCurrencyString()+"&thinsp;$</td>"+
+            "<td class='last-column price total-amount'>"+(product.price*product.quantity).toFixed(2).toCurrencyString()+"&thinsp;$</td>"+
           "</tr>"
       );
     sum+=product.price*product.quantity;
@@ -58,7 +58,7 @@ $(document).ready(function() {
         price = product.price;
         $(this).parent().find(".remove-quantity-button").prop('disabled',false);
         $(this).parent().find(".quantity").text(qty);
-        $(this).parent().parent().find("#total-amount").text((qty*price).toFixed(2).toCurrencyString()+" $");
+        $(this).parent().parent().find(".total-amount").text((qty*price).toFixed(2).toCurrencyString()+" $");
 
 
     });
@@ -74,7 +74,7 @@ $(document).ready(function() {
         if(qty==1){
             $(this).prop('disabled', true);
         }
-        $(this).parent().parent().find("#total-amount").text((qty*price).toFixed(2).toCurrencyString()+" $");
+        $(this).parent().parent().find(".total-amount").text((qty*price).toFixed(2).toCurrencyString()+" $");
     });
 
     $('#remove-all-items-button').click(function(){
