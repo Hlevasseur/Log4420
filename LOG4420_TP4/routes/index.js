f1a6417b87a1429d7883de417244650c9f5e18d6
fbeiger <fbeiger971@gmail.com>
2017-11-22 12:45:30 -0500

    refactor pugs, delete htmls, add routes to index

--- LOG4420_TP4/routes/index.js
+++ LOG4420_TP4/routes/index.js
@@ -5,4 +5,28 @@ router.get("/", function(req, res) {
   res.render("index", { page: "index" , title: "Online Shop - Accueil"});
 });
 
+router.get("/products", function(req, res) {
+  res.render("products", { page: "products" , title: "Online Shop - Produits"});
+});
+
+router.get("/product", function(req, res) {
+  res.render("products", { page: "products" , title: "Online Shop - Produit"});
+});
+
+router.get("/shopping-cart", function(req, res) {
+  res.render("shopping-cart", { page: "shopping-cart" , title: "Online Shop - Shopping Cart"});
+});
+
+router.get("/contact", function(req, res) {
+  res.render("contact", { page: "contact" , title: "Online Shop - Contact"});
+});
+
+router.get("/confirmation", function(req, res) {
+  res.render("confirmation", { page: "confirmation" , title: "Online Shop - Confirmation"});
+});
+
+router.get("/order", function(req, res) {
+  res.render("order", { page: "order" , title: "Online Shop - Commande"});
+});
+
 module.exports = router;
