153b5af255141492fb68d2d087d642bba0be218f
fbeiger <fbeiger971@gmail.com>
2017-12-01 14:42:02 -0500

    implement all methods in shopping-cart service

--- LOG4420_TP5/client/src/app/services/shopping-cart.service.ts
+++ LOG4420_TP5/client/src/app/services/shopping-cart.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, EventEmitter, Output } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { Config } from '../config';
-import { Product } from './products.service';
+import { Product, ProductsService } from './products.service';
 
 /**
  * Defines a ShoppingCart Product
@@ -19,6 +19,8 @@ class ShoppingCartProduct {
 export class ShoppingCartService {
 
   @Output() countUpdated: EventEmitter<number> = new EventEmitter();
+  private baseUrl = `${Config.apiUrl}/shopping-cart/`;
+  private options:RequestOptions = new RequestOptions({ headers: new Headers({ 'Content-Type': 'application/json' }), withCredentials: true });
 
   /**
    * Handles the current error.
@@ -38,19 +40,13 @@ export class ShoppingCartService {
    */
   constructor(private http: Http) { }
 
-  getOptions(): RequestOptions {
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    return new RequestOptions({ headers: headers, withCredentials: true });
-  }
-
   /**
    * Gets all the products in the shopping cart.
    *
    * @return {Promise<ShoppingCartProduct[]>}   The promise containing all the shoppingcartProducts inside.
    */
   getShoppingCart(): Promise<ShoppingCartProduct[]> {
-    let url = `${Config.apiUrl}/shopping-cart`;
-    return this.http.get(url, this.getOptions())
+    return this.http.get(this.baseUrl, this.options)
       .toPromise()
       .then(shoppingcartProducts => shoppingcartProducts.json() as ShoppingCartProduct[])
       .catch(ShoppingCartService.handleError);
@@ -64,14 +60,79 @@ export class ShoppingCartService {
   getCount(): Promise<number> {
     let self = this;
     return this.getShoppingCart()
-            .then(function(shoppingCartProducts) {
-              var count: number = 0;
-              shoppingCartProducts.forEach(function(shProduct) {
-                count = count + shProduct.quantity;
-              });
-              self.countUpdated.emit(count);
-              return count;
-            })
+      .then(function(shoppingCartProducts) {
+        var count: number = 0;
+        shoppingCartProducts.forEach(function(shProduct) {
+          count = count + shProduct.quantity;
+        });
+        self.countUpdated.emit(count);
+        return count;
+      });
+  }
+
+
+  /**
+   * Add a product to the cart.
+   *
+   * @param productId             The id of the product
+   * @param quantity              The quantity of the product
+   *
+   * @return {Promise<number>}    The Promise containing the response status
+   */
+  addProductToCart(productId, quantity): Promise<number> {
+    const body = {
+      productId: productId,
+      quantity: quantity
+    }
+    return this.http.post(this.baseUrl, body, this.options)
+      .toPromise()
+      .then(response => response.status);
+  }
+
+
+  /**
+   * Update the quantity of a product in cart.
+   *
+   * @param productId           The id of the product
+   * @param newQuantity         The new quantity of the product
+   *
+   * @return {Promise<number>}  The promise containing the response status
+   *
+   */
+  updateProductQuantity(productId, newQuantity): Promise<number> {
+    const url = this.baseUrl + productId;
+    const body = { quantity: newQuantity };
+    return this.http.put(url, body, this.options)
+      .toPromise()
+      .then(response => response.status);
+  }
+
+
+  /**
+   * Delete a product in cart.
+   *
+   * @param productId           The id of the product
+   *
+   * @return {Promise<number>}  The Promise containing the response status
+   *
+   */
+  deleteProduct(productId): Promise<number> {
+    const url = this.baseUrl + productId;
+    return this.http.delete(url, this.options)
+      .toPromise()
+      .then(response => response.status);
+  }
+
+
+  /**
+   * Delete the cart
+   *
+   * @return {Promise<number>}  The Promise containing the response status
+   */
+  deleteCart(): Promise<number> {
+    return this.http.delete(this.baseUrl, this.options)
+      .toPromise()
+      .then(response => response.status);
   }
 
 }
